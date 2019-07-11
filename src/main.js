0c7a783e0df4c85ae7b7692167dd3644734d4766
Luan Jesus <45802427+Nyfts@users.noreply.github.com>
2019-07-11 07:39:25 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
+function inicializar() {
+    var coordenadas = {lat: -23.087244, lng: -46.953936};
+ 
+    var mapa = new google.maps.Map(document.getElementById('mapa'), {
+      zoom: 15,
+      center: coordenadas 
+    });
+
+    var marker = new google.maps.Marker({
+      position: coordenadas,
+      map: mapa,
+      title: 'Meu marcador'
+    });
+}
+
+
+
+
+
 var isOnTop = true;
 
 
@@ -7,22 +26,32 @@ function changeNavPosition(){
         navbar.css({
             position : 'fixed',
             top: navbar.height()*-1 + 'px',
-            backgroundColor: 'rgba(101, 153, 143, 0.9)'
+            backgroundColor: 'rgba(101, 153, 143, 0.9)',
+            opacity: 0
         });
-        navbar.animate({top:0},200);
+        navbar.animate({
+            top:0,
+            opacity: 1
+        },200);
     } 
     else{
-        navbar.animate({top:'-60px'},200);
+        navbar.animate({
+            top:'-60px',
+            opacity: 0
+        },200);
         let delay = setInterval(function(){
             navbar.css({
                 position : 'absolute',
                 top: '0',
-                backgroundColor: 'rgba(0,0,0,0)'
+                backgroundColor: 'rgba(0,0,0,0)',
+                opacity: 1
             });
             clearInterval(delay);
         },300);
     }
 }
+
+
 
 
 window.onscroll = function(){
@@ -39,3 +68,21 @@ window.onscroll = function(){
         }
     }
 }
+
+
+
+
+$("#open-map").click(function(){
+    $("#open-map").animate({
+        opacity: 0
+    },400);
+    $("#mapa").animate({
+        height: "400px"
+    },400);
+    let abudaba = setInterval(function(){
+        $("#open-map").css('display','none');
+        clearInterval(abudaba);
+    },400)
+    
+});
+
