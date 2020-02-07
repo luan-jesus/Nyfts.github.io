8d1d31946b8d344d0a041f209047bf95beeecdde
Luan Jesus <luan.jesus@scopesystems.com.br>
2020-02-07 10:15:32 -0300

    ws

--- ws.js
+++ ws.js
@@ -8,3 +8,14 @@ xmlHttp.onreadystatechange = function() {
   xmlHttp.open("GET", "http://localhost:5000/api/score", true); // true for asynchronous 
   xmlHttp.send(null);
 }
+
+function createTable() {
+  // request = JSON.parse(JSONRequest)
+  var DOMtable = "<table><tr><td>Name</td><td>Score</td></tr>"
+  for (i = 0; i < request.length; i++) {
+    DOMtable += "<tr>";
+    DOMtable += "<td>"+request[i].name+"</td><td>"+request[1].value+"</td></tr>";
+  }
+  DOMtable += "</table>";
+  document.getElementById("body").innerHTML = DOMtable;
+}
